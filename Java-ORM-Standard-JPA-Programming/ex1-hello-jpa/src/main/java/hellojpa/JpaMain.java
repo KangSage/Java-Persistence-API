79a2d94dc7873f832f19dcb170fd90439a28f3f6
KangSage <ksage@knou.ac.kr>
2022-06-12 22:40:05 +0900

    entity manager flush

--- Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,19 +17,12 @@ public class JpaMain {
 
     tx.begin();
     try {
-      Member member = em.find(Member.class, 150L);
+      Member member = new Member(200L, "member200");
+      em.persist(member);
 
-      // em.persist()를 호출하지 않아도 영속성 컨텍스트에서 자동으로 저장된다.
-      member.setName("ZZZZZ");
-
-      // 아래아 같은 코드가 없어도 엔티티 객체의 값이 변경되면 커밋 될 때 자동으로 저장된다.
-      // if (member.getName().equals("ZZZZZ")) {
-      //   em.update(member);
-      // }
-
-      // em.persist(member);
-
-      // Dirty Checking으로 변경점 자동 업데이트 쿼리 실행
+      // 직접 호출
+      em.flush();
+      System.out.println("flush() 호출");
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
