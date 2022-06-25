76ea6e9c51ebf2ad7f2e9f67836e06acffacf5de
KangSage <ksage@knou.ac.kr>
2022-06-25 17:03:48 +0900

    DB 스키마 자동 생성

--- Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,8 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  * Entity 클래스에는 public or protected 기본 생성자가 필수<br>
@@ -10,10 +10,17 @@ import javax.persistence.Table;
  * - final 클래스, enum, interface, inner 클래스는 사용 불가 - 저장할 필드에 final 사용 X<br>
  */
 @Entity // JPA가 관리하는 객체 - DB 테이블과 매핑할 클래스에는 필수
-@Table(name = "MBR") // 실제 테이블과 Entity 객체의 이름이 다를 경우 테이블의 이름을 지정
 public class Member {
-  @Id private Long id;
+  @Id
+  private Long id;
+
+  // DDL 생성 기능 - JPA 실행 로직에는 영향 주지 않는다.
+  // length 등은 validation으로 동작은 한다.
+  @Column(unique = true, length = 10)
+
   private String name;
+
+  private int age;
 
   public Member() {}
 
