d78aa21b62b8174df1c61a34dcbc531c46891b30
KangSage <ksage@knou.ac.kr>
2022-06-25 16:36:44 +0900

    객체와 테이블 매핑

--- Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ Java-ORM-Standard-JPA-Programming/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,11 +2,17 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+/**
+ * Entity 클래스에는 public or protected 기본 생성자가 필수<br>
+ * - JPA가 리플렉션 API, Proxy 기술 등을 활용하여 객체의 기본 생성자를 사용한다.<br>
+ * - final 클래스, enum, interface, inner 클래스는 사용 불가 - 저장할 필드에 final 사용 X<br>
+ */
+@Entity // JPA가 관리하는 객체 - DB 테이블과 매핑할 클래스에는 필수
+@Table(name = "MBR") // 실제 테이블과 Entity 객체의 이름이 다를 경우 테이블의 이름을 지정
 public class Member {
-  @Id
-  private Long id;
+  @Id private Long id;
   private String name;
 
   public Member() {}
